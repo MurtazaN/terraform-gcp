f773460fccf9d63fc8fc01e2fb5282eabfc01bd8
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2026-03-24 02:55:41 -0400

    Update GCP provider, disable VM, add bucket
    Change GCP provider to project 'github-actions-485720' and switch region/zone from us-central1/a to us-east1/a. Comment out the google_compute_instance resource (disable VM provisioning) and replace the storage bucket resource with a new mlops-test-bucket located in us-east1. Add an initial terraform.tfstate file (empty state skeleton). These changes align resources with the us-east1 region and disable the VM while keeping the bucket configuration.

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 provider "google" {
-    project = "<YOUR_PROJECT_ID>" # Replace with your project ID
-    region  = "us-central1"
-    zone    = "us-central1-a"
+  project = "github-actions-485720" # Replace with your project ID
+  region  = "us-east1"
+  zone    = "us-east1-a"
 }
 
-resource "google_compute_instance" "vm_instance" {
-    name         = "terraform-vm"
-    machine_type = "f1-micro"
-    zone         = "us-central1-a"
+# resource "google_compute_instance" "mlops-test-vm" {
+#   name         = "mlops-test-vm"
+#   machine_type = "f1-micro"
+#   zone         = "us-east1-a"
 
-    labels = {
-        environment = "development"
-        owner = "team-terraform"
-    }
+#   labels = {
+#     environment = "development"
+#     owner       = "team-terraform"
+#   }
 
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-11"
-            size = 12
-        }
-    }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#       size  = 12
+#     }
+#   }
 
-    network_interface {
-        network = "default"
-    }
-}
+#   network_interface {
+#     network = "default"
+#   }
+# }
 
-resource "google_storage_bucket" "terraform-lab-bucket" {
-    name          = "terraform-lab-bucket-unique-12345"
-    location      = "us-central1"
-    force_destroy = true
+resource "google_storage_bucket" "mlops-test-bucket" {
+  name          = "mlops-test-bucket-unique"
+  location      = "us-east1"
+  force_destroy = true
 }
