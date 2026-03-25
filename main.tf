91721b7132428c8f9b489c24a2a80655590959d4
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2026-03-25 18:10:53 -0400

    Add Dockerfile + Flask app; enable Cloud Run
    Add a minimal Flask app (app.py) and Dockerfile, and update .gitignore to ignore tfvars files. Expand Terraform: declare required_providers, add a random_password resource to avoid hardcoding credentials, store the password in Secret Manager, and update the Cloud SQL instance/user/database to use the generated secret. Add a google_cloud_run_v2_service configured to mount the Cloud SQL socket and pass DB connection info via env vars (image is a placeholder for Artifact Registry). Add IAM bindings to make the Cloud Run service public and grant it access to the secret.

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 # Author: Murtaza N
 
 # THE CONNECTION (Provider GCP)
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
 provider "google" {
   project = "github-actions-485720"
   region  = "us-west1" # Using Oregon - it has high capacity
@@ -12,7 +23,8 @@ resource "google_project_service" "gcp_services" {
     "compute.googleapis.com",
     "sqladmin.googleapis.com",
     "secretmanager.googleapis.com",
-    "artifactregistry.googleapis.com"
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com"
   ])
   service            = each.key # ??
   disable_on_destroy = false
@@ -35,7 +47,6 @@ resource "google_compute_instance" "mlops-test-vm" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      #   size  = 12
     }
   }
 
@@ -53,8 +64,14 @@ resource "google_storage_bucket" "mlops-test-bucket" {
 
 # -----------------------------------------------------------------------------
 
+# GENERATE A SECURE RANDOM PASSWORD
+resource "random_password" "db_pwd" {
+  length           = 24
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?" # Avoid chars that can cause issues with connection strings
+}
 
-# THE VAULT (Secret Manager)
+# 3. THE VAULT (Secret Manager)
 resource "google_secret_manager_secret" "db_pwd" {
   secret_id  = "test-db-password"
   depends_on = [google_project_service.gcp_services]
@@ -65,12 +82,12 @@ resource "google_secret_manager_secret" "db_pwd" {
 
 resource "google_secret_manager_secret_version" "db_pwd_val" {
   secret      = google_secret_manager_secret.db_pwd.id
-  secret_data = "test_password_123" # The actual password
+  secret_data = random_password.db_pwd.result # Auto-generated, never hardcoded
 }
 
 # 4. THE DATABASE (Cloud SQL)
 resource "google_sql_database_instance" "postgres" {
-  name                = "mlops-test-db"
+  name                = "mlops-test-db-instance"
   database_version    = "POSTGRES_15"
   region              = "us-west1"
   deletion_protection = false
@@ -84,10 +101,88 @@ resource "google_sql_database_instance" "postgres" {
 resource "google_sql_user" "admin" {
   name     = "test-db-admin"
   instance = google_sql_database_instance.postgres.name
-  password = google_secret_manager_secret_version.db_pwd_val.secret_data
+  password = random_password.db_pwd.result # References the same generated password
 }
 
-# 5. THE WAREHOUSE (Artifact Registry)
+resource "google_sql_database" "mlops_db" {
+  name     = "mlops-test-db"
+  instance = google_sql_database_instance.postgres.name
+}
+
+# 5. THE RUNNER (Cloud Run)
+resource "google_cloud_run_v2_service" "mlops_app" {
+  name     = "mlops-app"
+  location = "us-west1"
+
+  depends_on = [google_project_service.gcp_services]
+
+  template {
+    # Connect to Cloud SQL
+    volumes {
+      name = "cloudsql"
+      cloud_sql_instance {
+        instances = [google_sql_database_instance.postgres.connection_name]
+      }
+    }
+
+    containers {
+      # TODO: Replace with your Artifact Registry image once built
+      # Format: us-west1-docker.pkg.dev/github-actions-485720/mlops-test-docker-repo/YOUR_IMAGE:TAG
+      image = "us-west1-docker.pkg.dev/github-actions-485720/mlops-test-docker-repo/test-app:v1"
+
+      ports {
+        container_port = 8080
+      }
+
+      # Pass DB connection info as env vars
+      env {
+        name  = "DB_USER"
+        value = google_sql_user.admin.name
+      }
+      env {
+        name = "DB_PASS"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.db_pwd.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name  = "DB_NAME"
+        value = google_sql_database.mlops_db.name
+      }
+      env {
+        name  = "INSTANCE_CONNECTION_NAME"
+        value = google_sql_database_instance.postgres.connection_name
+      }
+
+      # Mount the Cloud SQL proxy socket
+      volume_mounts {
+        name       = "cloudsql"
+        mount_path = "/cloudsql"
+      }
+    }
+  }
+}
+
+# Make Cloud Run publicly accessible (unauthenticated)
+# To make it private, remove this resource entirely
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  name     = google_cloud_run_v2_service.mlops_app.name
+  location = google_cloud_run_v2_service.mlops_app.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+# Grant Cloud Run access to read the secret
+resource "google_secret_manager_secret_iam_member" "cloudrun_secret_access" {
+  secret_id = google_secret_manager_secret.db_pwd.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_cloud_run_v2_service.mlops_app.template[0].service_account}" # Uses default compute SA
+}
+
+# 6. THE WAREHOUSE (Artifact Registry)
 resource "google_artifact_registry_repository" "repo" {
   location      = "us-west1"
   repository_id = "mlops-test-docker-repo"
